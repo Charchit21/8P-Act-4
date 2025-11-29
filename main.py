53925a1adec629b423ada73a59554a86d66b10de
Charchit21 <charchitberwal@gmail.com>
2025-11-29 18:47:10 +0530

    Union

---
+++ main.py
@@ -0,0 +1,8 @@
+my_set1 = {"Green", "Blue"}
+my_set2 = {"Blue", "Yellow"}
+print("Original Sets: ")
+print(my_set1)
+print(my_set2)
+print("/nUnion of two sets: ")
+setz = my_set1.union(my_set2)
+print(setz)
